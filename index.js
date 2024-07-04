2da3b05152267ab0560b9be79e7706ab2308d838
DoctorDinosaur <26852749+DoctorDinosaur@users.noreply.github.com>
2024-07-04 15:39:48 +0100

    Minimise to BBC

--- index.js
+++ index.js
@@ -2,53 +2,21 @@ import fs from "fs";
 import path from "path";
 import { schedule as cronSchedule } from "node-cron";
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
-import * as data from "./util/data.js";
 import { state } from "./util/state.js";
 import "dotenv/config";
-import { handleVoted, votedCommand } from "./server/voted.js";
 
 const __dirname = import.meta.dirname;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const channelid = process.env.CHANNEL_ID;
 
-client.commands = new Collection();
-client.data = data;
-
-export async function sendToNotificationChannels(message) {
-	const channels = await state.get("notification_channels");
-	
-	if (!channels) {
-		return;
-	}
-
-	for (const channel of channels) {
-		try {
-			const channelObj = await client.channels.fetch(channel.channel);
-			await channelObj.send(message);
-		} catch (error) {
-			console.error(`Failed to send message to channel ${channel.channel}: ${error}`);
-		}
+export async function sendToNotificationChannels(message) {	
+	try {
+		const channelObj = await client.channels.fetch(channelid);
+		await channelObj.send(message);
+	} catch (error) {
+		console.error(`Failed to send message to channel ${channelid}: ${error}`);
 	}
 }
-
-const foldersPath = path.join(__dirname, "commands");
-const folders = fs.readdirSync(foldersPath);
-
-for (const folder of folders) {
-    const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const { data, execute, autocomplete } = await import("file://" + filePath);
-		
-        if (data && execute) {
-			client.commands.set(data.name, { data, execute, autocomplete });
-		} else {
-			console.log(`[WARN] The command at ${filePath} is missing required "data" or "execute" property.`);
-		}
-	}
-}
-
-client.commands.set("voted", { execute: handleVoted, autocomplete: () => {}, data: votedCommand });
 
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
